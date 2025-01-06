18fd84ab9866fd8c1fcc2c9d78e2e5243c2d30ce
umiadore <umi.adore@gmail.com>
2025-01-07 01:39:07 +0400

    homework 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
-import Lesson01 from "./lessons/lesson01/Lesson01";
-import Lesson02 from "./lessons/lesson02/Lesson02";
+import "./homeworks/homework01/homework01.css";
+// import Lesson01 from "./lessons/lesson01/Lesson01";
+// import Lesson02 from "./lessons/lesson02/Lesson02";
+import Homework01 from "./homeworks/homework01/Homework01";
 
 function App() {
   return (
     <>
-      <Lesson01 />
-      <Lesson02 />
+      <Homework01 />
+      {/* <Lesson01 /> */}
+      {/* <Lesson02 /> */}
     </>
   );
 }
---
+++ src/homeworks/homework01/Homework01.jsx
@@ -0,0 +1,24 @@
+function Homework01() {
+    return (
+        <>
+            <div className="wrapper">
+                <div className = "grid-container">
+                    <div>
+                <img
+                src="https://media.licdn.com/dms/image/v2/D4D03AQFbqW8Df0MT5w/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1705521618805?e=1741824000&v=beta&t=gSYJOFGohV9V9WYxFmLRgD1R4R_3C2fuuEqr7Lv_WvE"
+                    alt="Picture"
+                />
+                <h3>Umut Kamaeva</h3>
+                <p>
+                    Hi, everyone! Ive always been fond of art but studied International
+                    Relations, and I currently work in Marketing. In addition, I love
+                    learning languages. So far, I speak four fluently. Java and JavaScript
+                    are in progress, as is Arabic. Life is a journey, lets see where ill
+                    land tomorrow. Yalla! 🌏
+                </p>
+            </div></div></div>
+        </>
+    );
+}
+
+export default Homework01;
